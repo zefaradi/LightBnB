afbc821f4256e9e65391bf89a1e23bbb6f4beb41
Adib Zefar <adibzefar@gmail.com>
2022-08-17 01:18:08 -0400

    Delete unused code

--- LightBnB_WebApp-master/LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/LightBnB_WebApp-master/server/database.js
@@ -162,10 +162,6 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  // const propertyId = Object.keys(properties).length + 1;
-  // property.id = propertyId;
-  // properties[propertyId] = property;
-  // return Promise.resolve(property);
 
     return pool
       .query(`INSERT INTO properties 
